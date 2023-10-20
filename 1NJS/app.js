ef8adad04c48fa280e586e7a4300a95a72074337
Andrii29102003 <71638275+Andrii29102003@users.noreply.github.com>
2023-10-20 21:49:51 +0000

    v.2b

--- 1NJS/app.js
+++ 1NJS/app.js
@@ -1,4 +1,9 @@
-const prompt = require('prompt-sync')();
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 
 function sortWordsByName(arr){
@@ -32,44 +37,57 @@ function showOnlyUniqueWords(arr){
 }
 
 //get values from user
-let values = prompt("Hi, please write 10 values separated by space: ").split(' ');
+rl.question("Hi, please write 10 values separated by space: ", (answer) => {
+    mainLoop(answer.split(" "));
+    rl.close();
+});
 
-//check if user wrote 10 values
-if(values.length !== 10){
-    console.log("You wrote wrong quantity of values, 10 values are required");
-    return;
-}
-//main loop
-while(true){
+//function mainLoop
+function mainLoop(values){
+    //check if user wrote 10 values
+    if(values.length !== 10){
+        console.log("You wrote wrong quantity of values, 10 values are required");
+        return;
+    }
     console.log("How would you like to sort values or write 'exit' to exit:");
     console.log("1. Words by name (from A to Z)");
     console.log("2. Show digits from the smallest");
     console.log("3. Show digits from the biggest");
     console.log("4. Words by quantyty of letters)");
     console.log("5. Only unique words");
-    let variant = prompt();
-    if(variant === 'exit'){
-        break;
-    }
+    rl.question("Your choice: ", (answer) => {
+        if(redirector(answer, values) === 1){
+            console.log("Goodbye!");
+            return;
+        }
+    });
+    console.log("BBBBBB");
+    return;
+}
 
-    //function redirector
+
+//function redirector
+function redirector(variant, values){
     switch(variant){
         case '1':
             console.log(sortWordsByName(values));
-            break;
+            return mainLoop(values);
         case '2':
             console.log(sortDigitsFromSmallest(values));
-            break;
+            return mainLoop(values);
         case '3':
             console.log(sortDigitsFromBiggest(values));
-            break;
+            return mainLoop(values);
         case '4':
             console.log(sortWordsByQuantityOfLetters(values));
-            break;
+            return mainLoop(values);
         case '5':
             console.log(showOnlyUniqueWords(values));
-            break;
+            return mainLoop(values);
+        case 'exit':
+            return 1;
         default:
             console.log("Wrong variant");
+            return mainLoop(values);
     }
 }
