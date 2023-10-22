5e83809d3764c5f308a2f05bd184301517bfd865
Andrii29102003 <71638275+Andrii29102003@users.noreply.github.com>
2023-10-22 12:35:43 +0000

    2NJS v.1

---
+++ 2NJS/app.js
@@ -0,0 +1,65 @@
+
+const fs = require("fs");
+const prompt = require("prompt-sync")();
+
+
+data = [];
+dbFile = "db.txt"; 
+
+function loadDB() {
+    try {
+        data = JSON.parse(fs.readFileSync(dbFile, "utf8"));
+    } catch (e) {
+        console.log("Error loading DB: " + e);
+        data = [];
+    }
+}
+
+function saveDB() {
+    try {
+        fs.writeFileSync(dbFile, JSON.stringify(data));
+    } catch (e) {
+        console.log("Error saving DB: " + e);
+    }
+}
+
+function addPerson(){
+    
+    person = {};
+
+    person.name = prompt("Enter the user`s name. To cancel press ENTER: ");
+    if(person.name === ""){
+        askToDo = prompt("Do you want to search data in database? Y/N: ");
+        if(askToDo === "Y"){
+            showDB();
+        }
+        return "doExit";
+    }
+    person.gender = prompt("Chose your gender: ");
+    person.age = parseInt(prompt("Enter your age: "));
+    
+    loadDB();
+    //add person to data
+    data.push(person);
+    saveDB();
+}
+
+function showDB(){
+    loadDB();
+    console.log(data);
+    askName = prompt("Enter the user`s name you want to search in database: ");
+    for(const person of data){
+        if(person.name === askName){
+            console.log(person);
+            return;
+        }
+    }
+    console.log("There is no such user in database");
+}
+
+
+while(true){
+    if(addPerson() === "doExit"){
+        break;
+    }
+}
