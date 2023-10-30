b3f54442a630567dcb6ebbc7aab9caf0332a67c2
Andrii29102003 <71638275+Andrii29102003@users.noreply.github.com>
2023-10-30 19:55:49 +0000

    v.09

--- 4NJS/app.js
+++ 4NJS/app.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const apiKey = "01767cd7f300e35d8433c5d83e3695c4"
 const city = "Vinnytsia";
 const generalURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
-const frequencyOfCheck = 1000; //10 min = 600000 ms
+const frequencyOfCheck = 600000; //10 min = 600000 ms
 
 const bot = new TelegramBot("6756857724:AAEtGxLZoyKTy4JsUid9aBSAQnhMtyl9F7k", {polling: true});
 var usersDB = [];
@@ -21,12 +21,9 @@ try{
 
 //button for chos each 4 hours or each 6 hours (hours*3600000)
 const options = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{ text: '4 hours', callback_data: 4*20000 }],
-            [{ text: '6 hours', callback_data: 6*20000 }],
-        ]
-    })
+    "reply_markup": {
+        "keyboard": [["4 hours"], ["6 hours"]]
+    }
 };
 
 
@@ -49,30 +46,46 @@ async function getWeather() {
 
 //if "/start" bot command is received add user to DB
 bot.onText(/\/start/, (msg) => {
-    console.log("start command received");//DEL
     //add options to bot
-    bot.sendMessage(msg.chat.id, "Choose frequency of weather send", options);
+    bot.sendMessage(msg.chat.id, "Choose how often you want to receive weather forecast:", options);
 });
-bot.on("callback_query", function (msg) {
-    bot.sendMessage(callbackQuery.message.chat.id, "You have chosen " + msg.data);
-    bot.answerCallbackQuery(msg.id, "You have chosen " + msg.data);
-    const selectedOption = msg.data;
-    const chatId = msg.chat.id;
-    user = {};
-    user.chatId = chatId;
-    user.selectedOption = selectedOption;
-    user.sendTime = Date.now() + selectedOption;
 
-    //load usersDB from file
-    usersDB = JSON.parse(fs.readFileSync(usersDBFileName));
-    //if user exists in DB remove him
-    usersDB = usersDB.filter(user => user.chatId !== chatId);
-    //add user to DB
-    usersDB.push(user);
-    //save DB to file
-    fs.writeFileSync(usersDBFileName, JSON.stringify(usersDB));
-
-    console.log("usersDB ", usersDB);//DEL
+bot.on('message', function (msg) {
+    if(msg.text === "4 hours"){
+        var user = {};
+        user.chatId = msg.chat.id;
+        user.selectedOption = 4*3600000;
+        user.sendTime = Date.now() + user.selectedOption;
+        //load usersDB from file
+        usersDB = JSON.parse(fs.readFileSync(usersDBFileName));
+        //if user exists in DB remove him
+        usersDB = usersDB.filter(userIterator => userIterator.chatId !== user.chatId);
+        //add user to DB
+        usersDB.push(user);
+        //save DB to file
+        fs.writeFileSync(usersDBFileName, JSON.stringify(usersDB));
+        bot.sendMessage(msg.chat.id, "You will receive weather forecast every 4 hours");
+    }
+    if(msg.text === "6 hours"){
+        user = {};
+        user.chatId = msg.chat.id;
+        user.selectedOption = 6*3600000;
+        user.sendTime = Date.now() + user.selectedOption;
+        //load usersDB from file
+        usersDB = JSON.parse(fs.readFileSync(usersDBFileName));
+        //if user exists in DB remove him
+        usersDB = usersDB.filter(userIterator => userIterator.chatId !== user.chatId);
+        //add user to DB
+        usersDB.push(user);
+        //save DB to file
+        fs.writeFileSync(usersDBFileName, JSON.stringify(usersDB));
+        bot.sendMessage(msg.chat.id, "You will receive weather forecast every 6 hours");
+    }
+    console.log("usersDB:", usersDB);//DEL
+    console.log("NOW =", Date(Date.now()));//DEL
+    for(userI of usersDB){
+        console.log("user.chatId", userI.chatId, "sendTime =", Date(userI.sendTime));//DEL
+    }
 });
 
 
@@ -81,15 +94,20 @@ async function sendForecast(){
     usersDB = JSON.parse(fs.readFileSync(usersDBFileName));
 
     //check if it is time to send weather
-    usersDB.forEach(async (user) => {
+    for (let user of usersDB) {
         if(Date.now() >= user.sendTime){
             //send weather
             data = await getWeather();
             bot.sendMessage(user.chatId, `Weather: ${data.weather}\nTemperature: ${data.temp}\nWind: ${data.wind}`);
             //update sendTime
             user.sendTime = Date.now() + user.selectedOption;
+            //convert time is seconds to uts
+
+
+            console.log("user.chatId", user.chatId, "NOW =", Date(Date.now()), "||", Date.now(), "new sendTime =", user.sendTime);//DEL
+            
         }
-    });
+    }
 
     //save DB to file
     fs.writeFileSync(usersDBFileName, JSON.stringify(usersDB));
@@ -98,6 +116,8 @@ async function sendForecast(){
     setTimeout(sendForecast, frequencyOfCheck);
 
 }
+
+bot.on("polling_error", console.log);
 
 //start sending forecast
 sendForecast();
