8f805e5ba1e81bbdd09089d43efdc402f9c4debd
Andrii29102003 <71638275+Andrii29102003@users.noreply.github.com>
2023-11-02 08:34:55 +0000

    v.01

---
+++ 5NJS/app.js
@@ -0,0 +1,60 @@
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+
+const TelegramAPIKey = '6965063003:AAF1SOHRZJd_yc0buDgmpAAd9p-EfL85YmU';
+const BankAPIURL = 'https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11';
+const options = {
+    reply_markup: {
+        keyboard: [
+            [{ text: "USD"}, { text: "EUR"}]
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: false
+    }
+};
+
+
+const bot = new TelegramBot(TelegramAPIKey, {polling: true});
+
+
+async function getExchangeRates(currency) {
+    const serverResponse = await axios.get(BankAPIURL);
+    let ansver = null;
+    for(let currenciVal of serverResponse.data) {
+        if(currenciVal.ccy == currency) {
+            ansver = currenciVal;
+        }
+    }
+    return ansver;
+}
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'Switch USD or EUR', options);
+});
+
+bot.onText(/(.+)/, (msg) => {
+    if(msg.text == 'USD') {
+        getExchangeRates('USD')
+            .then((response) => {
+                bot.sendMessage(msg.chat.id, `Currency: ${response.ccy}\nBuy: ${response.buy}\nSale: ${response.sale}`);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    if(msg.text == 'EUR') {
+        getExchangeRates('EUR')
+            .then((response) => {
+                bot.sendMessage(msg.chat.id, `Currency: ${response.ccy}\nBuy: ${response.buy}\nSale: ${response.sale}`);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+});
+
+
+
+
+bot.on("polling_error", console.log);
